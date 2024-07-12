321034c9e03dc0f6ad6253d9cf82697b4eb2a028
Angel Paul Patiño Diaz <146494692+Anyel-top@users.noreply.github.com>
2024-07-12 12:52:48 -0500

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SearchClientComponent } from './components/search-client/search-client.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'buscar-cliente', 
+        component: SearchClientComponent
+    }
+];
