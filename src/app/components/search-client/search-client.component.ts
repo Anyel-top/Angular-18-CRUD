735ec7c0fdcf576907706949c149dff0565debd7
Angel Paul Patiño Diaz <146494692+Anyel-top@users.noreply.github.com>
2024-07-15 11:48:34 -0500

    Add files via upload

--- src/app/components/search-client/search-client.component.ts
+++ src/app/components/search-client/search-client.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-client',
   standalone: true,
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './search-client.component.html',
   styleUrl: './search-client.component.scss'
 })
 export class SearchClientComponent {
+  public usuario:any;
+  public nombre: string = "";
+  public cedula: string = "";
+  public telefono: string = "";
+  public correo: string = "";
+  public direccion: string = "";
 
+  
 }
